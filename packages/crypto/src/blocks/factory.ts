5787836d1a47f1b71841094c440f607e51b1d140
mvictoria1997 <mvictoria1997@correo.ugr.es>
2020-11-13 13:03:31 +0100

    Sign lenght

--- packages/crypto/src/blocks/factory.ts
+++ packages/crypto/src/blocks/factory.ts
@@ -13,6 +13,7 @@ export class BlockFactory {
         const hash: Buffer = HashAlgorithms.sha256(payloadHash);
 
         data.blockSignature = Hash.signECDSA(hash, keys);
+		  console.log(data.blockSignature);
         data.id = Block.getId(data);
 
         return this.fromData(data);
